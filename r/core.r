8170e86498f6679db4bcabfb36d887842e0e76b5
Ben Carlson <ben.s.carlson@gmail.com>
2016-06-06 15:33:25 -0400

    Update core.r

--- r/core.r
+++ r/core.r
@@ -1,3 +1,4 @@
 cumsum(1:3) #this computes the cumulative sum in to a vector -> (1 3 6)
 rm(tmp) #unload object tmp from memory
+missing(x) #in a function, check to see if x has been passed in as a parameter
 
