8f9416ca10e52f32ff74e280211d04c3187d7f06
Ben Carlson <ben.s.carlson@gmail.com>
2016-06-07 16:24:46 -0400

    Create numpy.py

---
+++ python/numpy.py
@@ -0,0 +1,2 @@
+np.savetxt('myrecarray.csv',myrecarray, fmt='%s') #save myrecarray to csv file. need to specify fmt='%s' so it saves as strings
+myrecarray.dtype.names #print headers of the recarray
