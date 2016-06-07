343494c2be6dadda4308c9ae5c8df18490897826
Ben Carlson <ben.s.carlson@gmail.com>
2016-06-07 08:43:13 -0400

    Update core.r

--- r/core.r
+++ r/core.r
@@ -6,4 +6,6 @@ missing(x) #in a function, check to see if x has been passed in as a parameter
 treat <- as.factor(rep(c("A","B"),each=2))
 as.numeric(treat) # [1] 1 1 2 2
 
+class(x) #check the class of object x
+
 
