ac7bbfc833c69e822b2d613cb361c50c5beaf042
Ben Carlson <ben.s.carlson@gmail.com>
2016-06-07 08:23:09 -0400

    Update core.r

--- r/core.r
+++ r/core.r
@@ -2,3 +2,8 @@ cumsum(1:3) #this computes the cumulative sum in to a vector -> (1 3 6)
 rm(tmp) #unload object tmp from memory
 missing(x) #in a function, check to see if x has been passed in as a parameter
 
+#get the numeric levels for a factor
+treat <- as.factor(rep(c("A","B"),each=2))
+as.numeric(treat) # [1] 1 1 2 2
+
+
