725b4df86dc291b996d19a31eef78f04d4ed6fea
Ben Carlson <ben.s.carlson@gmail.com>
2016-06-10 15:40:08 -0400

    Update howto.sql

--- postgres/howto.sql
+++ postgres/howto.sql
@@ -1,1 +1,2 @@
-st_geogfromtext('SRID=4326;POINT(-77 38)'); #create a geography from POINT(-77 38). can leave out SRID=4326 and postgis will assume 4326.  good practice to make it explicit though.
+st_geogfromtext('SRID=4326;POINT(-77 38)'); --create a geography from POINT(-77 38). can leave out SRID=4326 and postgis will assume 4326.  good practice to make it explicit though.
+create index idx_t_geom on t using gist(geom) --create a spatial index on table t and column geom
