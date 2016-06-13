a0652b719437646d6f421807df6a528162adb818
Ben Carlson <ben.s.carlson@gmail.com>
2016-06-13 09:26:06 -0400

    Update howto.py

--- python/howto.py
+++ python/howto.py
@@ -1,1 +1,13 @@
 python --version #check python version
+
+range(8) #get a sequence of integers from 0 to 7
+range(3,7) #get a sequence of integers from 3 to 6
+range(2,11,2) #get a sequence of integers from 2 to 10, counting by 2
+range(10,1,-2) #get a sequence of integers from 10 to 2, counting down by 2
+
+li = [0,1,2,3,4]
+li[1:3] #returns [1,2]
+li[1:-1] #returns [1,2,3]
+
+
+
