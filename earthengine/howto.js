b546826e2f472f2ce67e4c217de0b563c95d626a
Ben Carlson <ben.s.carlson@gmail.com>
2016-06-13 18:08:27 -0400

    Update howto.js

--- earthengine/howto.js
+++ earthengine/howto.js
@@ -1,2 +1,5 @@
 reduceRegion(ee.Reducer.frequencyHistogram()) //compute number of distinct values in a histogram
 reduceRegion(ee.Reducer.sum().grouped(1)) //use raster zonal area (zonal stats) to realize sum, max value calculation
+
+var fc = (ee.FeatureCollection('ft:1Ec8IWsP8asxN-ywSqgXWMuBaxI6pPaeh6hC64lA')
+      .filter(ee.Filter().eq('ECO_NAME', 'Sonoran desert')));
