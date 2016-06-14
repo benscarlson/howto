8ea61c29463fb720792d59951ee418fc4b4540f9
Ben Carlson <ben.s.carlson@gmail.com>
2016-06-14 15:39:12 -0400

    Update spatial.r

--- r/spatial.r
+++ r/spatial.r
@@ -1,1 +1,8 @@
 as.data.frame(spdf) #convert SpatialPointsDataFrame spdf to a dataframe
+
+#raster package
+nlayers(mystack) #number of RasterLayers in the RasterStack
+names(mystack) #the names of the RasterLayers in the RasterStack
+extent(mystack) #extent of the RasterLayers in the RasterStack
+mystack[[1]] #extract the first RasterLayer from the RasterStack
+names(layer) <- 'layername' #set the name of RasterLayer to 'layername'
