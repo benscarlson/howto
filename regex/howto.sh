7e90c20b4d8b3929e357b05c8b0c9fedecb8f265
Ben Carlson <ben.s.carlson@gmail.com>
2016-06-16 21:13:00 -0400

    Update howto.sh

--- regex/howto.sh
+++ regex/howto.sh
@@ -1,1 +1,5 @@
+# ways that '^' is used
+^xyz #can indicate the beginning of a line 
+gr[^x]y #can indicate a "not" if used within a character class
+gr[^ae]y #applies to the whole list. this will not match gray or grey
 
