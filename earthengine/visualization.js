84f6b8d4054e1bdde374f4ceb0b7f840f492334c
Ben Carlson <ben.s.carlson@gmail.com>
2016-06-22 14:23:59 -0400

    Update visualization.js

--- earthengine/visualization.js
+++ earthengine/visualization.js
@@ -1,2 +1,5 @@
 //palettes
 var palette = ['b5acff','8dcfff','7ffff9','71ff96','c0ff6d','eeff64','ffc952','ffaf38','ff471d'];
+
+//paint a set of polygons polyFC (with identification 'id') to image and visualize
+Map.addLayer(ee.Image().int().paint(polyFC,'id').randomVisualizer())
