399a6311bd3ca578087a3052178434cc6031671b
Ben Carlson <ben.s.carlson@gmail.com>
2016-06-22 14:17:47 -0400

    Update howto.js

--- earthengine/howto.js
+++ earthengine/howto.js
@@ -6,3 +6,7 @@ var fc = (ee.FeatureCollection('ft:1Ec8IWsP8asxN-ywSqgXWMuBaxI6pPaeh6hC64lA')
 
 //add lon, lat bands to img      
 img = img.addBands(ee.Image.pixelLonLat());
+
+//paint a feature collection onto an image, color by the attribute "ID" (note: not tested)
+https://groups.google.com/d/msg/google-earth-engine-developers/_OBrauG-mQU/_tZL09nWbfwJ
+var img = ee.Image().byte().paint(fc, "ID");
