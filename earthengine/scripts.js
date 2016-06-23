9e529859487684035e7b5806b2fb6f75e76e8987
Ben Carlson <ben.s.carlson@gmail.com>
2016-06-23 10:31:28 -0400

    Create scripts.js

---
+++ earthengine/scripts.js
@@ -0,0 +1,4 @@
+//Generates a regular grid, randomly places a point within each grid
+//subject: How to create stratified sampling frame
+//https://groups.google.com/d/msg/google-earth-engine-developers/BaV5UwrN5H4/nGO1pWkNFQAJ
+https://code.earthengine.google.com/97af708e37b3b490bd592699014af86c
