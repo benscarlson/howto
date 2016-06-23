16229f7fe4c7280d13a47837466b5079874bc89b
Ben Carlson <ben.s.carlson@gmail.com>
2016-06-23 07:09:14 -0400

    Update howto.sh

--- regex/howto.sh
+++ regex/howto.sh
@@ -3,3 +3,5 @@
 gr[^x]y #can indicate a "not" if used within a character class
 gr[^ae]y #applies to the whole list. this will not match gray or grey
 
+cat|cattle #would match either cat or cattle
+ca(t|ttle) #would also match either cat or cattle
