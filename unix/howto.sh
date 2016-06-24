aaffc32a6be2bae0596c099617431dda31ee0e00
Ben Carlson <ben.s.carlson@gmail.com>
2016-06-24 07:44:45 -0400

    Update howto.sh

--- unix/howto.sh
+++ unix/howto.sh
@@ -13,6 +13,8 @@ dpkg -i debfile.deb #install a .deb file
 shopt - s dotglob
 mv dir1/* dir2
 
-tar xvf myfile.tar.gz #unzip up myfile.tar.gz
+tar xvf myfile.tar.gz #unzip myfile.tar.gz
+unzip file.zip -d my_folder #unzip a zip
+
 
 
