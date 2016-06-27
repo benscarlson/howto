113c6c2e469aba99984f61dae0a00ecf2dae78ca
Ben Carlson <ben.s.carlson@gmail.com>
2016-06-27 11:48:15 -0400

    Update scripts.js

--- earthengine/scripts.js
+++ earthengine/scripts.js
@@ -2,3 +2,7 @@
 //subject: How to create stratified sampling frame
 //https://groups.google.com/d/msg/google-earth-engine-developers/BaV5UwrN5H4/nGO1pWkNFQAJ
 https://code.earthengine.google.com/97af708e37b3b490bd592699014af86c
+
+//Compute number of tiles for a region - Matt Hancher
+//subject: Export.map - estimate number of tiles
+https://code.earthengine.google.com/c820de91ae30ce4aba068440043b0617?noload=1
