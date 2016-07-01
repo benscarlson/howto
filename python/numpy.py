7cc28445821113a529949d68aeab94d8d0a00983
Ben Carlson <ben.s.carlson@gmail.com>
2016-07-01 16:21:52 -0400

    Update numpy.py

--- python/numpy.py
+++ python/numpy.py
@@ -7,3 +7,8 @@ myrec.dtype.names #print headers of the recarray
 # comments='' so that it won't print a '#' at the beginning
 header = ','.join(myrec.dtype.names)
 np.savetxt('myrec.csv',myrec, fmt='%s', delimiter=',', header=header, comments='') 
+
+#open a csv file from disk and load into a recarray
+with open(csvFilePath,'rb') as csvfile:
+    csvrows = [row.replace('"', '') for row in csvfile]
+    feats = np.recfromcsv(csvrows, delimiter=',', autostrip=True)
