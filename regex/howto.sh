a47b8fc6f507b9b34dd36ea1ef88943eb9cf3153
Ben Carlson <ben.s.carlson@gmail.com>
2016-07-20 12:57:12 -0400

    Update howto.sh

--- regex/howto.sh
+++ regex/howto.sh
@@ -6,4 +6,7 @@ gr[^ae]y #applies to the whole list. this will not match gray or grey
 cat|cattle #would match either cat or cattle
 ca(t|ttle) #would also match either cat or cattle
 
+\d #match a single digit
+\d{1,2} #(I think) match one or two digits
+
 [^0-9] #match a non-digit character
