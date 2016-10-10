4e485eb551484a5d07865fe6a48a391c373594ff
Ben Carlson <ben.s.carlson@gmail.com>
2016-10-10 14:11:51 -0400

    Update dplyr.r

--- r/dplyr.r
+++ r/dplyr.r
@@ -1,1 +1,5 @@
 filter(!val %in% c('a','b')) #not in a, b
+
+dat %>% filter(complete.cases(.)) #only keep complete cases (rows with no NA)
+
+dat %>% summarise_each(funs(sum(is.na(.)))) #see how many na values are in each row
