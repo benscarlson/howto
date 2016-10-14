492147de11a85a07bd02a7969f4f789ccf894cf3
Ben Carlson <ben.s.carlson@gmail.com>
2016-10-14 19:24:41 -0400

    Update installation.r

--- r/installation.r
+++ r/installation.r
@@ -3,3 +3,10 @@
 # see second answer
 install.packages('hypervolume',dependencies=TRUE,repos='http://cran.rstudio.com/') #seems to install all dependencies, no matter what
 install.packages('hypervolume',repos='http://cran.rstudio.com/') #this is probably faster
+
+.libPaths() #see library paths. by default the first one is picked as the install location
+#path seem to be built from here file below. see: http://stackoverflow.com/questions/6218358/how-do-i-set-r-libs-site-on-ubuntu-so-that-libpaths-is-set-properly-for-all-u
+/etc/R/Renviron
+#to install into a 'local' directory, these is a line like:
+R_LIBS_USER=${R_LIBS_USER-'~/R/x86_64-pc-linux-gnu-library/3.2'}
+#if the folder ~/R/x86_64-pc-linux-gnu-library/3.2 exists, then when you start R it will be the default place to install libraries
