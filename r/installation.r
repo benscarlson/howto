17d69c386c6745ea42a89d3669bd3df97d731368
Ben Carlson <ben.s.carlson@gmail.com>
2016-10-14 18:58:32 -0400

    Update installation.r

--- r/installation.r
+++ r/installation.r
@@ -1,4 +1,5 @@
 #if r says something like 'not available for xyz version'
 #http://stackoverflow.com/questions/25721884/how-should-i-deal-with-package-xxx-is-not-available-for-r-version-x-y-z-wa
 # see second answer
-install.packages('hypervolume',dependencies=TRUE,repos='http://cran.rstudio.com/')
+install.packages('hypervolume',dependencies=TRUE,repos='http://cran.rstudio.com/') #seems to install all dependencies, no matter what
+install.packages('hypervolume',repos='http://cran.rstudio.com/') #this is probably faster
