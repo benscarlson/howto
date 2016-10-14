f9831cca412dccc84cbc844b26ca78dbe0e0d5e6
Ben Carlson <ben.s.carlson@gmail.com>
2016-10-14 18:41:28 -0400

    Update installation.r

--- r/installation.r
+++ r/installation.r
@@ -1,2 +1,4 @@
 #if r says something like 'not available for xyz version'
+#http://stackoverflow.com/questions/25721884/how-should-i-deal-with-package-xxx-is-not-available-for-r-version-x-y-z-wa
+# see second answer
 install.packages('hypervolume',dependencies=TRUE,repos='http://cran.rstudio.com/')
