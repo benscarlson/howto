fab85fee31169d0f9fe37b11d6cd52c9a036315b
Ben Carlson <ben.s.carlson@gmail.com>
2016-10-27 18:42:09 -0400

    Update ggplot2.r

--- r/ggplot2.r
+++ r/ggplot2.r
@@ -6,3 +6,25 @@ scale_color_gradient2('Legend title',low="red", mid='green', high='blue',
   midpoint=median(x),
   breaks=c(1,2,3,4),
   labels=c('one','two','three','four'))
+
+#set legend fonts
+theme(
+  legend.text = element_text(size = 8),
+  legend.title = element_text(size = 10))
+
+#----------
+# cowplot
+#----------
+
+#to set a blank theme, just use plot_grid. it will automatically set the theme to blank/classic
+
+#legends can really throw off plot_grid. It seems that legends aren't scaled correctly. 
+# so, the legend will be unscaled (often too big) and then the rest of the figure will be scaled
+# to fit the default size. This results in a figure that is way to small. To get around this, specifically 
+# set the base_width and base_height parameters in save_plot. Get the graph the way you want it,
+# then play around with the size of the legend by using theme(). Finally, make final adjustments to base_width and base_height
+
+save_plot('~/scratch/dualplot.png', dualPlot,
+          ncol = 2, nrow = 1,
+          base_width=8,
+          base_height=6)
