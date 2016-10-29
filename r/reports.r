2d2611ca222b769cf8b4551f0d99b09447681b60
Ben Carlson <ben.s.carlson@gmail.com>
2016-10-28 21:10:08 -0400

    Update reports.r

--- r/reports.r
+++ r/reports.r
@@ -5,7 +5,7 @@
 # to include latex in an rmarkdown file, just put the latex directly in the document
 # don't include the \begin{document} ... \end{document} tags.
 # also \usepackage needs to go in the YAML at the top:
-
+# http://tex.stackexchange.com/questions/171711/how-to-include-latex-package-in-r-markdown
 ---
 title: "Bavaria Static Niche"
 output: pdf_document
