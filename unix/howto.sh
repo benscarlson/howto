bcdd3c62cdf64458b81c2272a2196a40e1e9a3ec
Ben Carlson <ben.s.carlson@gmail.com>
2016-10-31 17:21:48 -0400

    Update howto.sh

--- unix/howto.sh
+++ unix/howto.sh
@@ -1,6 +1,7 @@
 nohup <command> & #what is the command for running a nohup job? 
 jobs OR jobs -l #if you are still logged into the same shell, use: 
-ps -ef | grep "<part of command name>" to find the pid #if you have exited out of the shell use
+ps -ef | grep "<part of command name>"# to find the pid #if you have exited out of the shell use this (but, which is the pid)?
+ps ax | grep "<part of command name>" #the first column is the pid
 kill -9 <pid> #to kill the process
 chown -R benc:benc data #change the user and group permissions for folder "data" to user: benc group: benc. also works on files
 
