313af2f5311f3a90f1d65cca64828742dfb0f6e1
Ben Carlson <ben.s.carlson@gmail.com>
2016-11-13 20:41:23 -0500

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,1 +1,8 @@
 
+#get the last commit (long version)
+git rev-parse HEAD
+git log --pretty=format:'%h' -n 1
+
+#get the last commit (long version)
+git rev-parse --short HEAD
+git log --pretty=format:'%H' -n 1
