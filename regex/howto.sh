56643adb652c9a05d57b2a3f860baa2ec9684fc2
Ben Carlson <ben.s.carlson@gmail.com>
2016-11-18 20:02:00 -0500

    Update howto.sh

--- regex/howto.sh
+++ regex/howto.sh
@@ -14,3 +14,7 @@ gr[ea]y #matches grey or gray
 gr[^x]y #matches anything but grxy.  eg. gray, grby, grcy
 [^0-9] #match a non-digit character
 
+$1 #refer back to a previous sub-group
+${1} #Use the second if there is a digit immediatly after the backreference. i.e. so the regex doesn't think you are trying to match $10, use ${1}0
+
+
