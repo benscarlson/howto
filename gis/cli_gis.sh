7071bce46fe8e6408dcb2e5f7476f4d7e564efb5
Ben Carlson <ben.s.carlson@gmail.com>
2016-11-21 15:18:24 -0500

    Update cli_gis.sh

--- gis/cli_gis.sh
+++ gis/cli_gis.sh
@@ -9,3 +9,8 @@ gdal_rasterize -burn 1 -l range -tr 0.00833333 0.00833333 range.shp range.tif
 listgeo -proj4 waterdistance.tif #another way to see metadata
 
 gdal_edit.py -a_srs EPSG:4326 myfile.tif #define projection when it is not set
+
+#metadata about a shapefile
+ogrinfo -ro -so -al myfile.shp #list important metadata
+#-al lists info on all layers, layer is not specified. to specify layer, use myfile.shp mylayer
+#-so: Summary Only: supress listing of features, show only the summary information like projection, schema, feature count and extents.
