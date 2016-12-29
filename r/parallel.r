d3a5f3b71b24f2fcc9e3196606a4654d101e2fee
Ben Carlson <ben.s.carlson@gmail.com>
2016-12-28 16:24:18 -0800

    Update parallel.r

--- r/parallel.r
+++ r/parallel.r
@@ -1,1 +1,6 @@
 
+detectCores() #show the number of cores (parallel package)
+
+cl <- makeCluster(4) #4 is the number of cores
+registerDoParallel(cl)
+on.exit(stopCluster(cl)) #need to use on.exit to stop the cluster when fn exits
