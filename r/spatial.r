8227ab45905715768d8ac73890062813f30098cd
Ben Carlson <ben.s.carlson@gmail.com>
2017-02-08 22:16:06 -0500

    Update spatial.r

--- r/spatial.r
+++ r/spatial.r
@@ -11,16 +11,22 @@ calc(layer,function(x) f(x)) #apply function f to layer
 cellStats(layer,sum) #returns the sum of all cells in layer. can also use mean, min, etc.
 metadata(layer) <- metadata #adds metadata to layer
 rdist_r@data@min;rdist_r@data@max #see min and max values for a rasterLayer
-
+writeRaster(env,'misc/tinamus_env.tif','GTiff') #write a raster to tif
+env_tif <- raster('misc/tinamus_env.tif') #load the raster from tif
+raster::extract(env_rdata,pts,df=T,ID=F) #extract raster values given a set of points (here, a SpatialPoints object)
+     
 getGDALVersionInfo()
+
+shapefile('myshapefile.shp') #part of raster package. load shapefile into a SpatialPolygonsDataFrame
 
 #create a SpatialPoints object. Not sure how to set the column names
 coords<-matrix(c(-45.1692940000,-23.4697250000),nrow=1)
 pts<-SpatialPoints(m, proj4string=CRS('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'))
+coordinates(df)=~Longitude+Latitude #turn df into a SpatialPointsDataFrame. set Longitude and Latitude as coordinates
 
-writeRaster(env,'misc/tinamus_env.tif','GTiff') #write a raster to tif
-env_tif <- raster('misc/tinamus_env.tif') #load the raster from tif
+zerodist(df) #find point pairs with equal spatial coordinates
+     
+#raster    
 
-raster::extract(env_rdata,pts,df=T,ID=F) #extract raster values given a set of points (here, a SpatialPoints object)
      
 bbox(obj) #get the bounding box of spatial object obj
