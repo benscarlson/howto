4adf9c30179f0a2801246920f2d7b04ab867cc61
Ben Carlson <ben.s.carlson@gmail.com>
2017-02-20 19:49:00 -0500

    Update hpc.sh

--- hpc.sh
+++ hpc.sh
@@ -1,1 +1,6 @@
 ssh bc447@omega.hpc.yale.edu #log in to omega
+qsub -q fas_devel -I #request interactive queue on fas_devel
+#omega queues: http://research.computing.yale.edu/support/hpc/clusters/omega
+module load Apps/R/3.3.2-generic #uses specific version
+module load Apps/R #uses default version
+module avail 
