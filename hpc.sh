0622fe9398cdda8eeee69358b18c978c947514fd
Ben Carlson <ben.s.carlson@gmail.com>
2017-02-24 16:57:16 -0500

    Update hpc.sh

--- hpc.sh
+++ hpc.sh
@@ -1,3 +1,13 @@
+### grace-next ###
+
+srun --pty -p interactive -c 1 -t 0:30:00 --mem-per-cpu=20000 bash #start an interactive session with 20GB of memory
+
+/gpfs/apps/hpc.rhel6/Apps/R/3.2.2-generic/lib/R/library #location of system r packages
+~/R/x86_64-pc-linux-gnu-library/3.2 #personal library
+
+
+
+### omega ###
 ssh bc447@omega.hpc.yale.edu #log in to omega
 qsub -q fas_devel -I #request interactive queue on fas_devel
 #omega queues: http://research.computing.yale.edu/support/hpc/clusters/omega
@@ -11,7 +21,3 @@ module avail
 #personal library
 /lustre/home/client/fas/jetz/bc447/R/x86_64-pc-linux-gnu-library/3.2/
 
-#NOT YET WORKING
-#install hypervolume package. requires RcppArmadillo, which requires gcc version >= 4.6
-module unload Libs/GCC/4.5.3
-module load Libs/GCC/5.2.0
