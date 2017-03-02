01655c99af739861d1b88fc896b991963f86836f
Ben Carlson <ben.s.carlson@gmail.com>
2017-03-02 12:30:53 -0500

    Update parallel.r

--- r/parallel.r
+++ r/parallel.r
@@ -4,3 +4,9 @@ detectCores() #show the number of cores (parallel package)
 cl <- makeCluster(4) #4 is the number of cores
 registerDoParallel(cl)
 on.exit(stopCluster(cl)) #need to use on.exit to stop the cluster when fn exits
+
+#MPI
+
+#close the cluster to avoid the error message about improper exit
+closeCluster(cl)
+mpi.quit()
