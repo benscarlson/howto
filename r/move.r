dfbf36a1d268e142a155434721aee71d378bc9b4
Ben Carlson <ben.s.carlson@gmail.com>
2017-04-07 10:47:37 -0400

    Update move.r

--- r/move.r
+++ r/move.r
@@ -1,2 +1,4 @@
 timestamps(mv) #get the timestamps of a move object
 timeLag(mv, units = 'mins')
+distance(mv) #returns vector of distances (in meters, for lon/lat) between points. vector length is one less than the number of points.
+angle(mv) #returns vector of angles between points. length is one less than number of points.
