8e04c157b86f62c03c1123fa6acb13a8dbfb4092
Ben Carlson <ben.s.carlson@gmail.com>
2017-05-08 09:37:00 -0400

    Update parallel.r

--- r/parallel.r
+++ r/parallel.r
@@ -1,12 +1,13 @@
 
 detectCores() #show the number of cores (parallel package)
+getDoParName() #this shows while parallel backend is registered
 
 cl <- makeCluster(4) #4 is the number of cores
 registerDoParallel(cl)
 on.exit(stopCluster(cl)) #need to use on.exit to stop the cluster when fn exits
 
 #MPI
-
+mpi.universe.size() #this should be the number of cores. on my laptop, it is 8
 #close the cluster to avoid the error message about improper exit
 closeCluster(cl)
 mpi.quit()
