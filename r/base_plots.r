277bb0285d1d3d3ee53a66de32d1cbe1144df642
Ben Carlson <ben.s.carlson@gmail.com>
2017-05-10 11:18:51 -0400

    Update base_plots.r

--- r/base_plots.r
+++ r/base_plots.r
@@ -2,3 +2,5 @@
 plot(data,col=data$myvar) #color by another variable
 text(data,labels=data$myid, cex= 0.7) #add labels to a plot
 lines(data) #add lines to a plot
+
+plot(density(rnorm(100))) #plot smooth density of a vector
