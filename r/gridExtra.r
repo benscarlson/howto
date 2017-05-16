7f6b4b886ddff82efe200ceb87fadcd8b75079c7
Ben Carlson <ben.s.carlson@gmail.com>
2017-05-16 15:47:41 -0400

    Update gridExtra.r

--- r/gridExtra.r
+++ r/gridExtra.r
@@ -1,7 +1,8 @@
-
+#---- draw to device ----
 grid.arrange(p1,p2,p3,ncol=3)
+grid.arrange(grobs=<listofgrobs>,ncol=3) #a list of grobs or gtables
 
-
+grid.draw(mygrob)
 
 #---- saving plots ----
 # grid.arrange draws directly to the device. http://stackoverflow.com/questions/17059099/saving-grid-arrange-plot-to-file
@@ -13,3 +14,5 @@ ggsave(file="whatever.pdf", g)
 pdf("filename.pdf")
   grid.arrange( graph1, graph2, ncol=2)
 dev.off()
+
+gtable_add_rows() #part of gtable package
