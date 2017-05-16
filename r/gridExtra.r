77ddceaddbeb8f557db4bd947b4b92fe31adab28
Ben Carlson <ben.s.carlson@gmail.com>
2017-05-16 15:00:15 -0400

    Update gridExtra.r

--- r/gridExtra.r
+++ r/gridExtra.r
@@ -1,6 +1,15 @@
 
 grid.arrange(p1,p2,p3,ncol=3)
 
+
+
+#---- saving plots ----
+# grid.arrange draws directly to the device. http://stackoverflow.com/questions/17059099/saving-grid-arrange-plot-to-file
+# arrangeGrob returns a grob object which can be saved using ggsave
+arrangeGrob(p1,p2,ncol=2)
+ggsave(file="whatever.pdf", g)
+
+# can also do the traditional way
 pdf("filename.pdf")
-grid.arrange( graph1, graph2, ncol=2)
+  grid.arrange( graph1, graph2, ncol=2)
 dev.off()
