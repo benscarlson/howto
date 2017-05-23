59970c12b0bfc05000eae74c6cf6c4bab93176cf
Ben Carlson <ben.s.carlson@gmail.com>
2017-05-23 11:50:46 -0400

    Update git.sh

--- git.sh
+++ git.sh
@@ -4,3 +4,4 @@ git rev-parse HEAD #5e152f31b4e0d4fe45403d822cd785da888e0431
 git log -1 --format='%H' #5e152f31b4e0d4fe45403d822cd785da888e0431
 git log --pretty --format='%h' -n 1 #5e152f3 (short version?)
 
+git clone git@github.com:repo.git myrepo #instead of cloning to folder 'repo' this will clone to folder 'myrepo'
