0db5f6fc5c5a3ba34bde2e9ec725753b0ccafaf8
Ben Carlson <ben.s.carlson@gmail.com>
2017-06-21 11:01:46 -0400

    Update core.r

--- r/core.r
+++ r/core.r
@@ -104,19 +104,6 @@ saveRDS(object,path) #save an object to an RDS file
 #infix operators
 `%s%` <- function(x,y) x + y
 1 %s% 2 #3
-
-#---- date and time ----
-      
-#GMT and UTC will always have the same time. However, UTC is a standard, while GMT is the actual timezone
-# in practice the times are the same. https://www.timeanddate.com/time/gmt-utc-time.html
-as.POSIXct('2014-09-14 19:45:09', tz='UTC') #store date and time, as number of seconds since Jan 1 1970. Usually the best choice for storage
-as.POSIXlt('2014-09-14 19:45:09', tz='UTC') #stores date and time as a list of elements
-as.POSIXct('2014-09-14 19:45:09') #This will assign the timezone to the current timezone as defined by the computer running the code
-
-strftime() #returns a character string
-### timing ###
-ptm <- proc.time() 
-elapsed_min <- round((proc.time() - ptm)[3]/60,2) #elapsed time in minutes
       
 ### *apply ###
 a<-1:3
