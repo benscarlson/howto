672c5ae5665da4a8c6765e79b7968ba715228018
Ben Carlson <ben.s.carlson@gmail.com>
2017-07-05 16:11:43 -0400

    Update spatial.r

--- r/spatial.r
+++ r/spatial.r
@@ -73,3 +73,4 @@ CRS("+proj=longlat") #WGS84
 CRS("+proj=longlat +ellps=WGS84") #WGS84
 CRS("+proj=longlat +datum=WGS84") #WGS84
 CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0") #WGS84
+CRS('+proj=aea +lat_1=43 +lat_2=62 +lat_0=30 +lon_0=10 +x_0=0 +y_0=0 +ellps=intl +units=m +no_defs') #albers
