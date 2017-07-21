296e72d897352c5806ccc63c4328de41788dbfd8
Ben Carlson <ben.s.carlson@gmail.com>
2017-07-21 12:31:21 -0400

    Update howto.js

--- earthengine/howto.js
+++ earthengine/howto.js
@@ -37,5 +37,9 @@ mycol = mycol.map(textTs);
 //filter an image collection using metadata field by list
 var col = ee.ImageCollection('MODIS/MOD11A2');
 var images = ['MOD11A2_005_2014_12_27','MOD11A2_005_2014_12_27'];
-var filt = ee.Filter.inList({leftField:'system:index',leftValue:null,rightField:null,rightValue:images});
 
+//filters have params in the following order:
+// (leftField, rightValue, rightField, leftValue)
+// below are equivilant
+var filt = ee.Filter.inList({leftField:'system:index',rightValue:images, rightField:null, leftValue:null});
+var filt = ee.Filter.inList(system:index',images);
