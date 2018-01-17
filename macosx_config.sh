b751d8294e3a5580d4a173171587b0972e281b5e
Ben Carlson <ben.s.carlson@gmail.com>
2018-01-17 15:50:37 -0500

    Update macosx_config.sh

--- macosx_config.sh
+++ macosx_config.sh
@@ -1,1 +1,25 @@
 
+#-------#
+# ruby -#
+#-------#
+
+# I had to update ruby so that homebrew would work.
+# Updated ruby based on instructions here: https://stackoverflow.com/questions/38194032/how-to-update-ruby-version-2-0-0-to-the-latest-version-in-mac-osx-yosemite
+
+\curl -sSL https://get.rvm.io | bash -s stable
+
+# Two notes (not sure if I need to do this)
+# * To start using RVM you need to run `source /Users/benc/.rvm/scripts/rvm`
+# in all your open shell windows, in rare cases you need to reopen all shell windows.
+#
+# * WARNING: '~/.profile' file found. To load it into your shell, add the following line to '/Users/benc/.bash_profile':#
+#
+#  source ~/.profile
+  
+rvm install ruby-2.4.2
+
+#------------#
+#--- brew ---#
+#------------#
+
+brew install miller
