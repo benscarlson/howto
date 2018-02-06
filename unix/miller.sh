11c659b527d7585a1349d8fc5ad5eea8c6089c87
Ben Carlson <ben.s.carlson@gmail.com>
2018-02-06 17:49:18 -0500

    Update miller.sh

--- unix/miller.sh
+++ unix/miller.sh
@@ -1,3 +1,22 @@
 
 mlr --csv filter '$behav == 4' sample.csv #filter
 mlr --csv --opprint --from sample.csv stats1 -a count -f bird_id -g bird_id #group by bird_id and count number of rows
+
+--csv #is a normal csv
+--icsv #is a csv in the form of field=value
+--ooprint #pretty prints the results to the screen
+-I #performs the file manipulation in place
+
+#---- creating new fields ----
+mlr --csv --opprint put -S '$type=typeof($bird_id)' sample_walk.csv #see the data type
+mlr --csv --opprint put -S '$tag_id=sub($bird_id, "^([0-9]{4})0([0-9]{3})$", "\1")' sample_walk.csv #pull out the first 4 digits
+# -S keeps the bird_id field as character and does not convert to number
+# \1 refers to the first group match
+mlr --csv put -S '$bird_id =~ "^([0-9]{4})0([0-9]{3})$"; $tag_id="\1"; $ring_id="\2"' sample_walk.csv #both tag_id and ring_id
+#note the =~ operator as the first item. Then, \1 and \2 group matches persist to the next column defintion.
+
+mlr --csv put '$timestamp = $date . "T" . $time . "Z"' sample_walk.csv #string concatenation
+
+#---- grouping ----
+#use to get a list of unique values in a column
+mlr --csv --from storks_4ben_walk.csv stats1 -a count -f tag_id -g tag_id #count, grouped by tag_id
