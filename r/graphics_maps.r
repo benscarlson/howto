4361a5b0bcd6a933c4e47865793c2aa52943117d
Ben Carlson <ben.s.carlson@gmail.com>
2018-03-13 13:02:37 -0400

    Update graphics_maps.r

--- r/graphics_maps.r
+++ r/graphics_maps.r
@@ -3,3 +3,9 @@ raster::scalebar(1000, type='bar', divs=4)
 
 ### plotting polygons ###
 splancs::polymap(mymatrix) #plot a polygon for a [,2] matrix
+
+#---- making maps ----#
+
+#make a scale bar. Need to have columns named "long" and "lat".
+  scalebar(data=rename(gdat,long=x,lat=y), dist=25, dd2km=TRUE, model='WGS84',location='bottomleft',
+    st.size=3, height=0.02) +
