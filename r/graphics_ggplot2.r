b3b67859c0c5a58e08993ee64e6493f945577dc2
Ben Carlson <ben.s.carlson@gmail.com>
2018-03-15 11:25:24 -0400

    Update graphics_ggplot2.r

--- r/graphics_ggplot2.r
+++ r/graphics_ggplot2.r
@@ -10,6 +10,15 @@ scale_color_gradient2('Legend title',low="red", mid='green', high='blue',
   breaks=c(1,2,3,4),
   labels=c('one','two','three','four'))
 
+#figure out breaks and format based on data. 
+#this is how ggplot internally figures out breaks
+#https://stackoverflow.com/questions/38486102/how-does-ggplot-calculate-its-default-breaks
+rng <- range(as.numeric(gdat$timestamp))
+breaks <-labeling::extended(rng[1], rng[2], m = 5) #note won't display the first and last items
+labels <- as.Date(as.POSIXct(breaks,origin='1970-01-01',tz='UTC'))
+
+p + scale_fill_gradient('Timestamp',low = "grey", high = "blue", breaks=breaks, labels=labels)
+
 # The order that items are drawn on the plot is based on the order of the data frame. 
 #   The first item is drawn first and the last item drawn last.
 
