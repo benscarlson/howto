c85fa99a68fecb0eb7e706d853321de80e950a74
Ben Carlson <ben.s.carlson@gmail.com>
2018-03-22 13:14:52 -0400

    Update howto.js

--- earthengine/howto.js
+++ earthengine/howto.js
@@ -50,9 +50,12 @@ ic.filter(ee.Filter.listContains("system:band_names", "N")) //filter by band nam
 Map.addLayer(img.select([0]).mask(msk),{palette:palette});
 
 //Visulications
-//palettes
-var palette = ['b5acff','8dcfff','7ffff9','71ff96','c0ff6d','eeff64','ffc952','ffaf38','ff471d'];
 
+//palettes
+//bright/aqua greenish to yellowish to red-orangeish
+var palette = ['b5acff','8dcfff','7ffff9','71ff96','c0ff6d','eeff64','ffc952','ffaf38','ff471d'];
+//EVI palette
+var palette = 'FFFFFF,CE7E45,DF923D,F1B555,FCD163,99B718,74A901,66A000,529400,3E8601,207401,056201,004C00,023B01,012E01,011D01,011301';
 //paint a set of polygons polyFC (with identification 'id') to image and visualize
 //example: https://code.earthengine.google.com/849862c672e8e5028a4d86ee14cf8c36
 Map.addLayer(ee.Image().int().paint(polyFC,'id').randomVisualizer())
