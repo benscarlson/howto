d7c9faaf1018e151f4ea33e87ddcd38a8d23211c
Ben Carlson <ben.s.carlson@gmail.com>
2018-03-27 16:29:38 -0400

    Update visualization.js

--- earthengine/visualization.js
+++ earthengine/visualization.js
@@ -1,5 +1,11 @@
-//palettes
+//---- palettes ----//
+
 var palette = ['b5acff','8dcfff','7ffff9','71ff96','c0ff6d','eeff64','ffc952','ffaf38','ff471d'];
+
+//this works as an ndvi/evi palette.
+var palette = 'FFFFFF,CE7E45,DF923D,F1B555,FCD163,99B718,74A901,66A000,529400,3E8601,207401,056201,004C00,023B01,012E01,011D01,011301';
+
+Map.addLayer(img.select('NDVI'), {palette:palette});
 
 //paint a set of polygons polyFC (with identification 'id') to image and visualize
 //example: https://code.earthengine.google.com/849862c672e8e5028a4d86ee14cf8c36
