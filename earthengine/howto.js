3471c8c5946f45c621bda8602e218c0d8607769e
Ben Carlson <ben.s.carlson@gmail.com>
2018-03-27 10:37:40 -0400

    Update howto.js

--- earthengine/howto.js
+++ earthengine/howto.js
@@ -1,3 +1,9 @@
+//---------------//
+//---- links ----//
+//---------------//
+
+//Data request? https://code.google.com/a/google.com/p/ee-testers-external/issues/list
+
 reduceRegion(ee.Reducer.frequencyHistogram()) //compute number of distinct values in a histogram
 reduceRegion(ee.Reducer.sum().grouped(1)) //use raster zonal area (zonal stats) to realize sum, max value calculation
 
