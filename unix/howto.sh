aa0fcffc700a937976727e05266a87a5a996719f
Ben Carlson <ben.s.carlson@gmail.com>
2018-06-01 19:19:27 -0400

    Update howto.sh

--- unix/howto.sh
+++ unix/howto.sh
@@ -98,9 +98,11 @@ http://localhost:631/printers
 
 #---- mount WD MyBook ----#
 # adapted from https://www.howtogeek.com/236055/how-to-write-to-ntfs-drives-on-a-mac/
-sudo umount /dev/disk2s1
+# https://github.com/osxfuse/osxfuse/issues/342 see here for recent sierra issue
+
+sudo umount /dev/disk3s1
 #ovolume and mount point names don't have to be the same. here, they are.
-sudo /usr/local/bin/ntfs-3g /dev/disk2s1 /Volumes/WD_MyBook_4TB -olocal -oallow_other -ovolname=WD_MyBook_4TB
+sudo /usr/local/bin/ntfs-3g /dev/disk3s1 /Volumes/WD_MyBook_4TB -olocal -oallow_other -ovolname=WD_MyBook_4TB
 sudo rm -r 'My Book' #clean up after automount might not need this, now mount point is being removed correctly
 
 #An A-Z Index of the Apple macOS command line (OS X)
