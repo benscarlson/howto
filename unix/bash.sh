888c97bf0e63bafef437822ba3bf9f3f990ec739
Ben Carlson <ben.s.carlson@gmail.com>
2018-06-05 14:29:33 -0400

    Update bash.sh

--- unix/bash.sh
+++ unix/bash.sh
@@ -2,3 +2,14 @@
 MYFOLDER='folder_name'
 ls $MYFOLDER #lists all files in folder_name
 echo ls $MYFOLDER #see the resulting command
+
+#--- loop through an array ---#
+declare -a arr=("50N_010E" "50N_010E" "60N_000E" "60N_010E")
+
+## now loop through the above array
+for i in "${arr[@]}"
+do
+   tar -xzvf Hansen_GFC2013_datamask_"$i".tif.zip --strip-components=6 Stornext/scienceweb1/development/gtc/downloads/WaterMask2010_UMD/Hansen_GFC2013_datamask_"$i".tif
+done
+
+tifs=`ls *.tif` #save the list of files to the tifs variable
