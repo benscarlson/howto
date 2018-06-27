72f74a5baf6932dec7946baacccf7335508c63bb
Ben Carlson <ben.s.carlson@gmail.com>
2018-06-27 14:17:25 -0400

    Update packages.r

--- r/packages.r
+++ r/packages.r
@@ -1,1 +1,6 @@
 
+#update namespace file (required to export functions)
+devtools::document()
+
+#note, if you get something like "... *.rdb is corrupt" restart r session and try again
+.rs.restartR()
