2b42c8e94e58319ee07d2ff2b35613b95081cb63
Ben Carlson <ben.s.carlson@gmail.com>
2018-07-06 13:50:09 -0400

    Update string_manipulation.r

--- r/string_manipulation.r
+++ r/string_manipulation.r
@@ -1,2 +1,7 @@
-#---- strings ----
+
+#---- extracting substrings ----
+stringr::str_extract('123ABC','\\d+') #will extract '123'. seems to work better than gsub
+stringr::str_sub('123ABC',1,3) #First three chars '123'
+stringr::str_sub('123ABC',-3,-1) #Third from last to last 'ABC'
+
 sub('([^ ]+)_(\\d{4}$)','\\1','Adi_2_2017') #will print out the first matched group
