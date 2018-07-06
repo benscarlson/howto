26b5d2b85586c07f634112ff0b15aab7549132b4
Ben Carlson <ben.s.carlson@gmail.com>
2018-07-06 14:15:00 -0400

    Update string_manipulation.r

--- r/string_manipulation.r
+++ r/string_manipulation.r
@@ -5,3 +5,6 @@ stringr::str_sub('123ABC',1,3) #First three chars '123'
 stringr::str_sub('123ABC',-3,-1) #Third from last to last 'ABC'
 
 sub('([^ ]+)_(\\d{4}$)','\\1','Adi_2_2017') #will print out the first matched group
+
+#---- reverse string ----
+stringi::stri_reverse('123ABC') 
