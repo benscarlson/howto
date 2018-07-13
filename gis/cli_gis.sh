820c26e552c3894a20e0dff788ac49b651b3103a
Ben Carlson <ben.s.carlson@gmail.com>
2018-07-13 16:19:36 -0400

    Update cli_gis.sh

--- gis/cli_gis.sh
+++ gis/cli_gis.sh
@@ -25,10 +25,11 @@ gdal_edit.py -a_srs EPSG:4326 myfile.tif #define projection when it is not set
 #---- transform rasters ----
 
 #mosiac rasters
-gdal_merge.py -pct -n 0 -a_nodata 0 -o out.tif in1.tif in2.tif
+gdal_merge.py -pct -n 0 -a_nodata 0 -co COMPRESS=DEFLATE -o out.tif in1.tif in2.tif
 -pct #this takes the color table from the first input and applies to to output
 -n 0 #this tells merge to ignore these values from input rasters (i.e. treat as no data)
 -a_nodata #this tells merge to set 0 as the no data value in the output
+-co #This writes the raster as a compressed file. Compressed might be 400MB, uncompressed 6GB!
 
 #convert to wgs84
 gdalwarp -t_srs '+proj=longlat +datum=WGS84 +no_defs' image.tif image_wgs84.tif
