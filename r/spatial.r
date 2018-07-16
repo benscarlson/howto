b8481a77d6b57140c0fd437fc7e25150afef3b58
Ben Carlson <ben.s.carlson@gmail.com>
2018-07-16 15:42:28 -0400

    Update spatial.r

--- r/spatial.r
+++ r/spatial.r
@@ -181,3 +181,7 @@ CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0") #WGS84
 CRS('+proj=aea +lat_1=43 +lat_2=62 +lat_0=30 +lon_0=10 +x_0=0 +y_0=0 +ellps=intl +units=m +no_defs') #albers
 CRS('+proj=utm +zone=33 +ellps=WGS84 +units=m +no_defs') #UTM zone 33N
 CRS('+proj=utm +zone=33 +south +ellps=WGS84 +datum=WGS84 +units=m +no_defs') #UTM zone 33S (note +south)
+
+#proj4 notes
+#+towgs84=0,0,0 the last portion of each proj4 string is +towgs84=0,0,0 . This is a conversion factor that is used if a datum conversion is required.
+# this is from (http://bit.ly/2utPyH7) and I don't know what it's supposed to mean.
