4160b55effdeaabfff6093d4084a764a228b461c
Ben Carlson <ben.s.carlson@gmail.com>
2018-07-18 11:44:18 -0400

    Update packages.r

--- r/packages.r
+++ r/packages.r
@@ -4,3 +4,6 @@ devtools::document()
 
 #note, if you get something like "... *.rdb is corrupt" restart r session and try again
 .rs.restartR()
+
+#get the full path of a specified file in a package
+system.file("external/test.grd", package="raster")
