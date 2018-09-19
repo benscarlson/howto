d82a415fdc14230092dbfd28cf1d1c537715ba0b
Ben Carlson <ben.s.carlson@gmail.com>
2018-09-19 11:24:36 -0400

    Update graphics_maps.r

--- r/graphics_maps.r
+++ r/graphics_maps.r
@@ -12,3 +12,9 @@ splancs::polymap(mymatrix) #plot a polygon for a [,2] matrix
 #make a scale bar. Need to have columns named "long" and "lat".
   scalebar(data=rename(gdat,long=x,lat=y), dist=25, dd2km=TRUE, model='WGS84',location='bottomleft',
     st.size=3, height=0.02) +
+
+#use x.min, etc. to manually set the bounds. scalebar is placed relative to this box. note here data=NULL
+  ggsn::scalebar(data=NULL,
+      dist = 50, dd2km = TRUE, model = 'WGS84', location='bottomleft',
+      height=0.03, st.size=3, st.dist=0.05, st.color='white', #st.color requires dev version ggsn
+      x.min=bb['x.min'], x.max=bb['x.max'], y.min=bb['y.min'], y.max=bb['y.max'])
