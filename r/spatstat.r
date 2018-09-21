878d6c7b911daf9818451f9ff9b29d1a74be618e
Ben Carlson <ben.s.carlson@gmail.com>
2018-09-21 13:58:03 -0400

    Update spatstat.r

--- r/spatstat.r
+++ r/spatstat.r
@@ -2,6 +2,7 @@
 #---- import data ----#
 
 ppp1 <- as.ppp(SpatialPoints(spdf1)) #as.ppp is in maptools package. spdf1 is a SpatialPointsDataFrame
+unitname(ppp1) <- c('degree','degrees') #set unit names
 
 #see here for example importing a point dataset into spatstat
 #http://rstudio-pubs-static.s3.amazonaws.com/5273_fbda8993a4904f7e8c64e6c3da5ce7c6.html
