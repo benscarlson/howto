00ec8221d7db72af0d0fea8483ab9e0c541b446b
Ben Carlson <ben.s.carlson@gmail.com>
2018-09-21 13:56:45 -0400

    Update spatstat.r

--- r/spatstat.r
+++ r/spatstat.r
@@ -2,3 +2,6 @@
 #---- import data ----#
 
 ppp1 <- as.ppp(SpatialPoints(spdf1)) #as.ppp is in maptools package. spdf1 is a SpatialPointsDataFrame
+
+#see here for example importing a point dataset into spatstat
+#http://rstudio-pubs-static.s3.amazonaws.com/5273_fbda8993a4904f7e8c64e6c3da5ce7c6.html
