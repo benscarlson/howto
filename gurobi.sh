3ac25474c4f367b3865233e1507d719cb9d924a5
Ben Carlson <ben.s.carlson@gmail.com>
2018-09-23 13:19:49 -0400

    Update gurobi.sh

--- gurobi.sh
+++ gurobi.sh
@@ -1,1 +1,12 @@
+#---- installation 
 
+#Uninstall old version by dragging gurobi.app to the trash
+#Install latest version from package
+#Get a free academic license
+# Go to web site: http://www.gurobi.com/documentation/8.0/quickstart_mac/creating_a_new_academic_li.html#subsection:createacademiclicense
+# Agree to terms
+# Copy the grbgetkey command
+# Log into VPN if necessary
+# Run the command
+
+exec gurobi.sh #starts up gurobi shell (also prints version)
