dd8f4581918cd9b257fc77d3bcee0500957d872d
Ben Carlson <ben.s.carlson@gmail.com>
2018-10-11 22:31:02 -0400

    Update howto.js

--- earthengine/howto.js
+++ earthengine/howto.js
@@ -12,6 +12,14 @@
 //haven't tried this, but should be how to clone my repo to local
 git clone https://earthengine.googlesource.com/users/benscarlson/default
 
+//---- CLI ----
+cd to the folder containing the layer
+the "layer" is the folder that contains the shp and associated files
+
+gsutil -m cp -r UTM_Zone_Boundaries gs://benc/export_to_ee
+
+earthengine upload table gs://benc/export_to_ee/UTM_Zone_Boundaries/UTM_Zone_Boundaries.shp --asset_id users/benscarlson/ref/utmzones
+
 //--------------------//
 //---- geometries ----//
 //--------------------//
