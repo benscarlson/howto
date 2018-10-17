5cf64e113ee223e09d91689188eece149cb7db0a
Ben Carlson <ben.s.carlson@gmail.com>
2018-10-17 12:57:55 -0400

    Update cli.sh

--- earthengine/cli.sh
+++ earthengine/cli.sh
@@ -1,6 +1,7 @@
 //---- CLI ----
-cd to the folder containing the layer
-the "layer" is the folder that contains the shp and associated files
+
+#---- upload a shapefile ----
+#cd to the folder containing the layer. the "layer" is the folder that contains the shp and associated files
 
 gsutil -m cp -r UTM_Zone_Boundaries gs://benc/export_to_ee
 
