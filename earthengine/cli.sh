4f41f7ff382434c9afc014977573def0461b8cd2
Ben Carlson <ben.s.carlson@gmail.com>
2018-10-18 14:43:13 -0400

    Update cli.sh

--- earthengine/cli.sh
+++ earthengine/cli.sh
@@ -6,3 +6,6 @@
 gsutil -m cp -r UTM_Zone_Boundaries gs://benc/export_to_ee
 
 earthengine upload table gs://benc/export_to_ee/UTM_Zone_Boundaries/UTM_Zone_Boundaries.shp --asset_id users/benscarlson/ref/utmzones
+
+#---- download from gcs
+gsutil -m cp -r gs://benc/scratch/utmtest/* .
