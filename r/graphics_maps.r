1de26e41146c2075e60246d0d0682377aaabdae5
Ben Carlson <ben.s.carlson@gmail.com>
2018-10-25 11:23:05 -0400

    Update graphics_maps.r

--- r/graphics_maps.r
+++ r/graphics_maps.r
@@ -18,3 +18,15 @@ splancs::polymap(mymatrix) #plot a polygon for a [,2] matrix
       dist = 50, dd2km = TRUE, model = 'WGS84', location='bottomleft',
       height=0.03, st.size=3, st.dist=0.05, st.color='white', #st.color requires dev version ggsn
       x.min=bb['x.min'], x.max=bb['x.max'], y.min=bb['y.min'], y.max=bb['y.max'])
+
+#---- ggmap ----
+
+#-- api key
+#need to have development version of ggmap (2.7.900)
+#use devtools to install
+
+#Example static maps call:
+#https://maps.googleapis.com/maps/api/staticmap?center=waco+texas&zoom=12&size=640x640&scale=2&maptype=terrain&key=<mykey>
+
+#Check api usage?
+#https://console.cloud.google.com/google/maps-apis/overview?onboard=true&project=research-1537904108006&consoleUI=CLOUD
