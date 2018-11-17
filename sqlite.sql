8d5fd72016283b4e4de0b3b090a0f23582a800ea
Ben Carlson <ben.s.carlson@gmail.com>
2018-11-17 15:08:43 -0500

    Update sqlite.sql

--- sqlite.sql
+++ sqlite.sql
@@ -1,1 +1,22 @@
 
+INTEGER PRIMARY KEY - This will reference the rowid, an autoincrementing integer that is automatically created 
+
+--import if file does not contain a file header
+.import data/derived/player_table_init.csv player
+
+--There is no flag to skip file header when importing. so, need to explicitly skip it
+.import "|tail -n +2 data/derived/player_table_init.csv" player
+
+.headers on     --display headers when running select statement
+.schmea mytable --display table schema
+
+PRAGMA foreign_keys         --check if foreign keys are enabled
+PRAGMA foreign_keys = ON;   --Turn on
+PRAGMA foreign_keys = OFF;  --Turn off
+
+-- to insert data into a table that has an autoincrement pk
+-- first, create a temp table (with col1, col2)
+-- then, create the target table (with pk, col1, col2)
+-- finally, insert from temp into target
+INSERT INTO fd_stats(pk, col1, col2) 
+SELECT * FROM fd_stats_temp;
