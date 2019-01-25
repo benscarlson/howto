7dc7fceca94ecec3f9111adbfd09eeaa35739500
Ben Carlson <ben.s.carlson@gmail.com>
2019-01-25 14:50:14 -0500

    Update miller.sh

--- unix/miller.sh
+++ unix/miller.sh
@@ -1,3 +1,4 @@
+#http://johnkerl.org/miller/doc/10-min.html
 
 mlr --csv filter '$behav == 4' sample.csv #filter
 mlr --csv --opprint --from sample.csv stats1 -a count -f bird_id -g bird_id #group by bird_id and count number of rows
