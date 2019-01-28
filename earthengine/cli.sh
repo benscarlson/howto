6181555dd7e271000ff0c679eb067ffcb0aa4820
Ben Carlson <ben.s.carlson@gmail.com>
2019-01-28 12:10:36 -0500

    Update cli.sh

--- earthengine/cli.sh
+++ earthengine/cli.sh
@@ -7,5 +7,8 @@ gsutil -m cp -r UTM_Zone_Boundaries gs://benc/export_to_ee
 
 earthengine upload table gs://benc/export_to_ee/UTM_Zone_Boundaries/UTM_Zone_Boundaries.shp --asset_id users/benscarlson/ref/utmzones
 
+#---- delete assets ----#
+earthengine rm -r projects/map-of-life/benc/l8sr_ndvi_loburg_sum14_new --dry_run #use dry_run to see what would be deleted
+
 #---- download from gcs
 gsutil -m cp -r gs://benc/scratch/utmtest/* .
