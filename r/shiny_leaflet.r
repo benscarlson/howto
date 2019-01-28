91279eb1e1997d53b615b879ecbef089b7cd86c9
Ben Carlson <ben.s.carlson@gmail.com>
2019-01-28 12:13:43 -0500

    Create shiny_leaflet.r

---
+++ r/shiny_leaflet.r
@@ -0,0 +1,2 @@
+# Dynamic Leaflet maps in Shiny applications. https://riptutorial.com/r/example/16144/dynamic-leaflet-maps-in-shiny-applications
+# Shiny articles - reactivity. https://shiny.rstudio.com/articles/#reactivity
