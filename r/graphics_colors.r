1eeb788c6a0637b232f35a8d8f94d1c6a2cc0ac3
Ben Carlson <ben.s.carlson@gmail.com>
2019-01-31 15:42:50 -0500

    Update graphics_colors.r

--- r/graphics_colors.r
+++ r/graphics_colors.r
@@ -1,19 +1,19 @@
-#NOTE: moved these into bencmisc/colorfuns.r
+#NOTE: moved commented code into bencmisc/colorfuns.r
 
 # Convert 3 digit tuple of RBG to hex. https://stackoverflow.com/questions/31574480/rgb-to-hex-converter
-x <- c("165 239 210", "111 45 93")
-sapply(strsplit(x, " "), function(x) {
-    rgb2(x[1], x[2], x[3], maxColorValue=255)})
-
-rgb2hex <- function(r,g,b) rgb(r, g, b, maxColorValue = 255)#rgb2hex(255,0,0). https://gist.github.com/mbannert/e9fcfa86de3b06068c83
-
-#turn colorname into hex. https://gist.github.com/mbannert/e9fcfa86de3b06068c83
-col2hex <- function(col, alpha) rgb(t(col2rgb(col)), alpha=alpha, maxColorValue=255)
-col2hex('firebrick') # [1] "#B22222"
-col2hex('firebrick', 204) # [1] "#B22222CC"
-
-#working with a color palettes
-https://drsimonj.svbtle.com/creating-corporate-colour-palettes-for-ggplot2
+#x <- c("165 239 210", "111 45 93")
+#sapply(strsplit(x, " "), function(x) {
+#    rgb2(x[1], x[2], x[3], maxColorValue=255)})
+#
+#rgb2hex <- function(r,g,b) rgb(r, g, b, maxColorValue = 255)#rgb2hex(255,0,0). https://gist.github.com/mbannert/e9fcfa86de3b06068c83
+#
+##turn colorname into hex. https://gist.github.com/mbannert/e9fcfa86de3b06068c83
+#col2hex <- function(col, alpha) rgb(t(col2rgb(col)), alpha=alpha, maxColorValue=255)
+#col2hex('firebrick') # [1] "#B22222"
+#col2hex('firebrick', 204) # [1] "#B22222CC"
+#
+##working with a color palettes
+#https://drsimonj.svbtle.com/creating-corporate-colour-palettes-for-ggplot2
 
 #-- make color palette from picture
 # https://datascienceplus.com/how-to-use-paletter-to-automagically-build-palettes-from-pictures/
