1be2c36ce52d97d5924d2f5b66a2167ae9ba0487
Ben Carlson <ben.s.carlson@gmail.com>
2019-02-22 12:09:49 -0500

    Update cli.sh

--- earthengine/cli.sh
+++ earthengine/cli.sh
@@ -3,9 +3,9 @@
 #---- upload a shapefile ----
 #cd to the folder containing the layer. the "layer" is the folder that contains the shp and associated files
 
-gsutil -m cp -r UTM_Zone_Boundaries gs://benc/export_to_ee
+gsutil -m cp -r UTM_Zone_Boundaries gs://mol-playground/benc/ingest_ee
 
-earthengine upload table gs://benc/export_to_ee/UTM_Zone_Boundaries/UTM_Zone_Boundaries.shp --asset_id users/benscarlson/ref/utmzones
+earthengine upload table gs://mol-playground/benc/ingest_ee/UTM_Zone_Boundaries/UTM_Zone_Boundaries.shp --asset_id users/benscarlson/ref/utmzones
 
 #---- delete assets ----#
 earthengine rm -r projects/map-of-life/benc/l8sr_ndvi_loburg_sum14_new --dry_run #use dry_run to see what would be deleted
