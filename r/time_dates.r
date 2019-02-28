ccdd3c84d14948625b310a647ad29dd63cb1b837
Ben Carlson <ben.s.carlson@gmail.com>
2019-02-28 14:25:49 -0500

    Update time_dates.r

--- r/time_dates.r
+++ r/time_dates.r
@@ -18,9 +18,15 @@ s1 <- as.POSIXct('2017-09-07 00:00:00', tz='US/Eastern')
 s2 <- as.POSIXct('2017-09-07', tz='US/Eastern')
 identical(s1,s2) #TRUE
 
+.POSIXct(as.integer(Sys.time()),tz='UTC') #This converts timestamp to UTC
+
+strftime() #returns a character string. make sure to specify timezone, or local system timezone is assumed!
+strftime('2017-04-27',format='%Y-%m-%dT%TZ', tz='GMT') #standard format
+
 # %T and %H:%M:%S are the same thing
-strftime(startDate-seconds(1),format='%Y-%m-%dT%T', usetz=TRUE, tz='US/Eastern')
-strftime(startDate-seconds(1),format='%Y-%m-%dT%H:%M:%S', usetz=TRUE, tz='US/Eastern')
+strftime(strTS,format='%Y-%m-%dT%T', usetz=TRUE, tz='US/Eastern')
+strftime(strTS,format='%Y-%m-%dT%H:%M:%S', usetz=TRUE, tz='US/Eastern')
+strftime(strTS,format='%Y-%m-%dT%H:%M:%S', tz='US/Eastern') #usetz prints out the tz in the result: 2014-06-01T03:20:11 UTC
 
 #convert from unix epoch time
 as.POSIXct(1417305600,tz='UTC',origin='1970-01-01') #always use origin as 1970-01-01
@@ -30,8 +36,7 @@ as.POSIXct('2017-06-27', tz='US/Eastern') #class(): "POSIXct" "POSIXt" print(): 
 as.Date('2017-06-27', tz='US/Eastern') # class(): "Date". print(): "2017-06-27"
 OlsonNames() #get the names of timezones for the tz attribute
 
-strftime() #returns a character string. make sure to specify timezone, or local system timezone is assumed!
-strftime('2017-04-27',format='%Y-%m-%dT%TZ', tz='GMT') #standard format
+
 =TEXT(C2,"yyyy-mm-ddThh:MM:ssZ") #convert excel date to ISO formatted timestamp
 
 #---- lubridate ----
