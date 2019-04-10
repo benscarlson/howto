0faeea3a8d2d34597b2219fa3d1ca86d7c0b33ad
Ben Carlson <ben.s.carlson@gmail.com>
2019-04-10 16:57:26 -0400

    Update howto.sh

--- unix/howto.sh
+++ unix/howto.sh
@@ -10,6 +10,7 @@ dpkg -i debfile.deb #install a .deb file
 #---- listing files ----#
 
 ls -lh #human-readable file sizes
+du -h mydir #total size of directory
 
 #Reading output from ls
 # This basically says, interpreting this from RIGHT to LEFT that the file, linux_course_notes.txt was created at 6:30 PM on July 10 and is 1892 bytes large. 
