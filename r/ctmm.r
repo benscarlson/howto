133035a416c943c4f9bb2ae5f7649a3572019bdd
Ben Carlson <ben.s.carlson@gmail.com>
2019-05-09 15:29:38 -0400

    Update ctmm.r

--- r/ctmm.r
+++ r/ctmm.r
@@ -1,1 +1,7 @@
-
+#convert dataframe to telemetry object
+dat0 %>%
+  filter(niche_name=='Magnus-2014') %>%
+  arrange(timestamp) %>%
+  select(location.long=lon,location.lat=lat,timestamp,individual.local.identifier=niche_name) %>%
+  as.data.frame %>%
+  as.telemetry()
