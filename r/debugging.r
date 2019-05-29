a9a4ffac10d5bc9fa36c3f2e4c4425578e5eb47b
Ben Carlson <ben.s.carlson@gmail.com>
2019-05-29 10:18:07 -0400

    Create debugging.r

---
+++ r/debugging.r
@@ -0,0 +1,20 @@
+#https://jozef.io/r916-exploring-r-code-interactively/
+
+debug(order) #when order() is called, r will break into the debugger
+undebug(order) #remove debug call
+debugonce(order) #will just debug once, no need to call undebug()
+
+debugonce(blogdown:::list_rmds) #debug an unexported function
+
+body(stats::aggregate) #this will list the code of the function
+
+#this will call the data.frame version of aggregate, instead of the generic form
+eval(debugcall(
+  aggregate(mtcars["hp"], mtcars["carb"], FUN = mean),
+  once = TRUE
+))
+
+#stop a function at a specific line of code
+as.list(body(stats::aggregate.data.frame)) #see all lines of code from aggregate
+trace(stats::aggregate.data.frame, tracer = browser, at = 21) #insert a debug call at line 21
+untrace(stats::aggregate.data.frame) #remove the trace
