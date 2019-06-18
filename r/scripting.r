1e36dfda44cc55a2d33fc0be04172fd07980885a
Ben Carlson <ben.s.carlson@gmail.com>
2019-06-18 13:37:20 -0400

    Update scripting.r

--- r/scripting.r
+++ r/scripting.r
@@ -3,8 +3,11 @@
 R CMD BATCH test.r #run file test.R from the command line
 R --slave -f test.r #also run from the command line
 Rscript <script_name.r>
+
 #add this to top of script to run directly from the command line
 #!/usr/bin/env Rscript
+#make executable
+chmod 744 myscript.r #this will result in rwxr--r--
 
 # get commandline arguments within a script
 args <- commandArgs(trailingOnly=TRUE)
