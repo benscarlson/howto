4fae8ccf1428e15c3f7441a009e4ca5fa095f559
Ben Carlson <ben.s.carlson@gmail.com>
2019-06-18 13:31:58 -0400

    Update scripting.r

--- r/scripting.r
+++ r/scripting.r
@@ -3,8 +3,12 @@
 R CMD BATCH test.r #run file test.R from the command line
 R --slave -f test.r #also run from the command line
 Rscript <script_name.r>
+#add this to top of script to run directly from the command line
+#!/usr/bin/env Rscript
 
 # get commandline arguments within a script
 args <- commandArgs(trailingOnly=TRUE)
 datName <- args[1]
 if(is.na(datName)) stop('datName required')
+
+
