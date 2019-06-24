5eed4894bc726849e7250a2fec33ed97e71af1de
Ben Carlson <ben.s.carlson@gmail.com>
2019-06-24 13:14:11 -0400

    Update configuration.sh

--- python/configuration.sh
+++ python/configuration.sh
@@ -1,2 +1,9 @@
 
 sudo pip install earthengine-api --upgrade #upgrade a package
+
+pip show earthengine-api #show version
+pip freeze | grep earthengine-api #version using older versions of pip
+
+pip search earthengine-api #check for newer version (lots of output, look at top of output)
+pip list -o | grep earthengine-api #also checks for newer version
+
