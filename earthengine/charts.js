968304d1f8ce6671c71dfa6619353af796d97776
Ben Carlson <ben.s.carlson@gmail.com>
2019-06-28 11:42:09 -0400

    Update charts.js

--- earthengine/charts.js
+++ earthengine/charts.js
@@ -1,2 +1,5 @@
 //Example of mean NDVI over time (Noel). Subject 'Adding a line Chart'
-https://code.earthengine.google.com/4d3fa33c1ab76f8531387189b6ae999f
+//https://code.earthengine.google.com/4d3fa33c1ab76f8531387189b6ae999f
+
+//Example of UI panel, charts, and table
+//https://code.earthengine.google.com/008dcf204d94c06c5a42fbfce71fd7b6
