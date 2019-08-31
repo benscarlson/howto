a7e361f234374c620493617ee6e63268dece2041
Ben Carlson <ben.s.carlson@gmail.com>
2019-08-31 15:09:20 -0400

    Update ctmm.r

--- r/ctmm.r
+++ r/ctmm.r
@@ -5,3 +5,12 @@ dat0 %>%
   select(location.long=lon,location.lat=lat,timestamp,individual.local.identifier=niche_name) %>%
   as.data.frame %>%
   as.telemetry()
+
+#AKDE
+#akde can take a telemetry object and best.fit? Maybe this will internally fit model?
+AKDES <- akde(telem.obj, best.fit)
+#can also do like this, giving it a fitted model:
+animal.akde <- akde(tel, mod, res = 50)
+#also seems akde can take a list of objects and not just one at a time
+#https://groups.google.com/d/msg/ctmm-user/_gMRESokCYc/MY9ShphIBwAJ
+#might be required if I'm doing overlap metrics
