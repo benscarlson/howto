acc5ae3ea13b9ed6a42aa0da06ac4daa50104eab
Ben Carlson <ben.s.carlson@gmail.com>
2019-10-11 11:38:35 -0400

    Update database.r

--- r/database.r
+++ r/database.r
@@ -17,3 +17,19 @@ sumLong %>%
   mutate(model_summary_id=NA) %>% #add PK column set to NA
   select(model_summary_id,everything()) %>% #move to the right position
   dbAppendTable(db, "model_summary", .)
+
+#---- updating data ----#
+
+#updating is hard. need to update row by row
+for(i in seq_len(nrow(sqldat))) {
+  #i <- 2
+  row <- sqldat[i,]
+  sql <- glue_sql(
+    "update niche_set_vol 
+    set nestedness={row$nestedness} 
+    where niche_set={row$niche_set} 
+    and hv_job_name={row$hv_job_name}",.con=db)
+  q <- dbSendQuery(db,sql)
+  
+  dbClearResult(q) #need to clear response buffer or I get warning
+}
