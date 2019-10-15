2e04eed559afed844ce84b6648e50a4711cddc47
Ben Carlson <ben.s.carlson@gmail.com>
2019-10-15 11:27:20 -0400

    Update database.r

--- r/database.r
+++ r/database.r
@@ -18,18 +18,25 @@ sumLong %>%
   select(model_summary_id,everything()) %>% #move to the right position
   dbAppendTable(db, "model_summary", .)
 
+dbExecute(db,sql) #use to execute query that does not return data (e.g. update)
+dbSendQuery(db,sql) #can be used to update, but can also be used to make request need to clear result no matter what
+dbClearResult(q) #need to clear response buffer after calling dbSendQuery
+
 #---- updating data ----#
 
 #updating is hard. need to update row by row
-for(i in seq_len(nrow(sqldat))) {
-  #i <- 2
-  row <- sqldat[i,]
+for(i in seq_len(nrow(rini))) {
+  #i <- 1
+  row <- rini[i,]
   sql <- glue_sql(
-    "update niche_set_vol 
-    set nestedness={row$nestedness} 
-    where niche_set={row$niche_set} 
+    "update indiv_vol 
+    set rini={row$rini} 
+    where niche_name={row$niche_name} 
     and hv_job_name={row$hv_job_name}",.con=db)
-  q <- dbSendQuery(db,sql)
   
-  dbClearResult(q) #need to clear response buffer or I get warning
+  af <- dbExecute(db,sql)
+  
+  #should update exactly 1 row, if not provide warning
+  if(af != 1) message(glue('Warning, update for {row$niche_name} affected {af} rows'))
 }
+
