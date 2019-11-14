96e30f0c61311847e63f639ee364562f495e9585
Ben Carlson <ben.s.carlson@gmail.com>
2019-11-14 08:58:55 -0500

    Update ctmm.r

--- r/ctmm.r
+++ r/ctmm.r
@@ -31,6 +31,30 @@ plot(v,CTMM=m, xlim=c(0,2 %#% "hour"))
 plot(vg,CTMM=mod,fraction=0.65)
 plot(vg,CTMM=mod,fraction=1) #full variogram, but first 50% is generally helpful
 
+#---- autocorrelation decay ----#
+# From chris "tau_p = 6 days does not mean that the variogram asymptotes at 6 days. 
+# This is an exponential decay timescale, and so for instance you get 95% decay at 3*tau_p"
+
+# Also from chris:
+# "tau_velocity is approximately the timescale over which correlations in velocity decay exponentially. 
+# Velocity autocorrelation decays with an envelope like exp(-lag/tau_velocity). 
+# So when only 5% of velocity autocorrelation is remaining you have 0.05=exp(-lag/tau_velocity) which 
+# you can solve to find  lag=-log(0.05)*tau_velocity."
+
+# so, if we have a given amount of autocorrelation we can solve for lag
+# acor = exp(-lag/tau_v)
+# log(acor) = -lag/tau_v
+# lag = tau_v * -log(acor)
+
+#also note that at the point lag = tau_v, we have:
+# acor = exp(-tau_v/tau_v)
+# acor = exp(-1)
+# acor ~ 37%
+# at lag = tau_v, we have 37% autocorrelation remaining
+# and 100-37% = 63% decay in autocorrelation
+
+#based on above, the exponential decay function is valid for both tau_v and tau_p
+
 #when is velocity 95% uncorrelated?
 #note that -log(0.05) is around 3
 m$tau['velocity'] * -log(0.05) #note velocity is in SI units, so this is seconds
@@ -50,6 +74,16 @@ title('95% decay in position autocorrelation')
 
 ls(ctmm:::plot.env) #can see other items in plot env
 
+#---- Evaluating model fit ----#
+
+# From Chris: https://groups.google.com/forum/#!topic/ctmm-user/Blcg7gb-aAY
+# "But also for this specific variogram, I would be careful to make sure that telemetry error or non-stationarity 
+# aren't problematic because the fitted model is only nailing the first lag. The sampling rate seems to be ~5 minutes. 
+# With larger species, the finer the sampling, the bigger problem unmodeled telemetry error becomes for underestimating 
+# tau_velocity."
+
+# In plot, semivariance of model is higher than empirical semi-variance for most of the plot
+# So, this means an underestimate of tau_v results in overestimate of semi-variance
 #---- ctmm objects ----#
 
 #-- ctmm model
