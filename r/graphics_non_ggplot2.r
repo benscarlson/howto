bd56e02b9a534cff7a71c0e0395f044960175ece
Ben Carlson <ben.s.carlson@gmail.com>
2019-11-25 15:35:52 -0500

    Update graphics_non_ggplot2.r

--- r/graphics_non_ggplot2.r
+++ r/graphics_non_ggplot2.r
@@ -6,6 +6,8 @@
 #---- colors ----#
 #----------------#
 
+#coolors. site to build color palette: https://coolors.co/def2c8-ff101f-7c90db-c5dac1-bcd0c7
+
 col=rainbow(length(x)) #rainbow palette for base R plot. x is a vector.
 
 #---- ggsci ----#
