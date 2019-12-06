9cebcfcf88ff428c8d0cb7aa6a5e5680fef2dcef
Ben Carlson <ben.s.carlson@gmail.com>
2019-12-06 11:34:00 -0500

    Update hypervolume.r

--- r/hypervolume.r
+++ r/hypervolume.r
@@ -1,1 +1,11 @@
+#---- Usage Questions ----#
+# When comparing hypervolumes, should I use a different bandwidth per hypervolumes, 
+# or should I use the same bandwidth for all hypervolumes?
 
+ceiling((10^(3 + sqrt(ncol(dat))))/nrow(dat)) #default for samples.per.point
+
+#Jaacard index
+hvset <- hypervolume_set(hv1,hv2,check.memory = FALSE)
+
+hypervolume_overlap_statistics(hvset) #This will have Jaacard, plus others
+hvset@HVList$Intersection@Volume / hvset@HVList$Union@Volume #Can compute exactly as well
