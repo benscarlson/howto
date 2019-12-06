5e6b668c3f79d6a481aa0f4b8d72d8a3ab7f474d
Ben Carlson <ben.s.carlson@gmail.com>
2019-12-06 11:34:54 -0500

    Update hypervolume.r

--- r/hypervolume.r
+++ r/hypervolume.r
@@ -4,8 +4,8 @@
 
 ceiling((10^(3 + sqrt(ncol(dat))))/nrow(dat)) #default for samples.per.point
 
-#Jaacard index
+#Jaccard index
 hvset <- hypervolume_set(hv1,hv2,check.memory = FALSE)
 
-hypervolume_overlap_statistics(hvset) #This will have Jaacard, plus others
+hypervolume_overlap_statistics(hvset) #This will have Jaccard, plus others
 hvset@HVList$Intersection@Volume / hvset@HVList$Union@Volume #Can compute exactly as well
