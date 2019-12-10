518d4b63a36e2bfc3e0cd1515b084266d6dbb04c
Ben Carlson <ben.s.carlson@gmail.com>
2019-12-10 15:15:39 -0500

    Update graphics_ggplot2.r

--- r/graphics_ggplot2.r
+++ r/graphics_ggplot2.r
@@ -10,24 +10,8 @@ geom_text_repel(data=dat, aes(x=lon, y=lat,label=study_num),size=3,force=3) + #s
 #---- geoms ----#
 ggConvexHull::geom_convexhull() #convex hull
 
-#----
-#---- color ----#
-#----
-
-#Note setting manual colors and applying to multiple aesthetics
-cols <- c('random'='#0073C2FF','actual'='#FC4E07')
-ggplot(gdat,aes(x=abs(diff),fill=diff_type,color=diff_type,..scaled..)) +
-  geom_density(alpha=0.5) +
-  scale_fill_manual(values=cols,aesthetics=c('color','fill'))
-
-#create a three part color gradient
-scale_color_gradient2('Legend title',low="red", mid='green', high='blue',
-  midpoint=median(x),
-  breaks=c(1,2,3,4),
-  labels=c('one','two','three','four'))
-
-#-- viridis
-scale_color_viridis(discrete=TRUE) #for factor, omit discrete for continuous
+#---- colors ----#
+# See colors.r
 
 #figure out breaks and format based on data. 
 #this is how ggplot internally figures out breaks
