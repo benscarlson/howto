05e5ed6d071ffb37bbd8ad74c673bb5d505966a5
Ben Carlson <ben.s.carlson@gmail.com>
2019-12-10 15:13:08 -0500

    Update graphics_non_ggplot2.r

--- r/graphics_non_ggplot2.r
+++ r/graphics_non_ggplot2.r
@@ -2,23 +2,8 @@
 #http://gforge.se/2013/02/exporting-nice-plots-in-r/
 #http://bit.ly/2JrDbRW. Producing a vector graphics image (i.e. metafile) in R suitable for printing in Word 2007
 
-#----------------#
 #---- colors ----#
-#----------------#
-
-#coolors. site to build color palette: https://coolors.co/def2c8-ff101f-7c90db-c5dac1-bcd0c7
-
-col=rainbow(length(x)) #rainbow palette for base R plot. x is a vector.
-
-#---- ggsci ----#
-#https://nanx.me/ggsci/articles/ggsci.html
-#Has color palettes for popular journals and sci-fi shows
-
-#---- viridis ----#
-viridis::viridis(12)
-
-#Turbo, An Improved Rainbow Colormap for Visualization
-#https://ai.googleblog.com/2019/08/turbo-improved-rainbow-colormap-for.html
+# See colors.r
 
 #-----------------------#
 #---- base graphics ----#
