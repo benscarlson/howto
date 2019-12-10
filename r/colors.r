f7a3ce3c38ef312831498f670b305fb210f1c730
Ben Carlson <ben.s.carlson@gmail.com>
2019-12-10 15:14:59 -0500

    Update colors.r

--- r/colors.r
+++ r/colors.r
@@ -16,3 +16,22 @@ viridis::viridis(12)
 
 #Turbo, An Improved Rainbow Colormap for Visualization
 #https://ai.googleblog.com/2019/08/turbo-improved-rainbow-colormap-for.html
+
+#----------------#
+#---- ggplot ----#
+#----------------#
+
+#Note setting manual colors and applying to multiple aesthetics
+cols <- c('random'='#0073C2FF','actual'='#FC4E07')
+ggplot(gdat,aes(x=abs(diff),fill=diff_type,color=diff_type,..scaled..)) +
+  geom_density(alpha=0.5) +
+  scale_fill_manual(values=cols,aesthetics=c('color','fill'))
+
+#create a three part color gradient
+scale_color_gradient2('Legend title',low="red", mid='green', high='blue',
+  midpoint=median(x),
+  breaks=c(1,2,3,4),
+  labels=c('one','two','three','four'))
+
+#-- viridis
+scale_color_viridis(discrete=TRUE) #for factor, omit discrete for continuous
