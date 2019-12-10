ad6d55bfcf62d742e2500f44faf83a8ff9960fc6
Ben Carlson <ben.s.carlson@gmail.com>
2019-12-10 15:13:32 -0500

    Update colors.r

--- r/colors.r
+++ r/colors.r
@@ -1,1 +1,18 @@
 
+#----------------#
+#---- colors ----#
+#----------------#
+
+#coolors. site to build color palette: https://coolors.co/def2c8-ff101f-7c90db-c5dac1-bcd0c7
+
+col=rainbow(length(x)) #rainbow palette for base R plot. x is a vector.
+
+#---- ggsci ----#
+#https://nanx.me/ggsci/articles/ggsci.html
+#Has color palettes for popular journals and sci-fi shows
+
+#---- viridis ----#
+viridis::viridis(12)
+
+#Turbo, An Improved Rainbow Colormap for Visualization
+#https://ai.googleblog.com/2019/08/turbo-improved-rainbow-colormap-for.html
