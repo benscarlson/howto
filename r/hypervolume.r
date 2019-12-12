41d06d5db2bf453bd759f2c68780820caf5b2f23
Ben Carlson <ben.s.carlson@gmail.com>
2019-12-12 12:00:35 -0500

    Update hypervolume.r

--- r/hypervolume.r
+++ r/hypervolume.r
@@ -7,5 +7,8 @@ ceiling((10^(3 + sqrt(ncol(dat))))/nrow(dat)) #default for samples.per.point
 #Jaccard index
 hvset <- hypervolume_set(hv1,hv2,check.memory = FALSE)
 
+#recommended number of observations is:
+# obs > exp(number of axes)
+
 hypervolume_overlap_statistics(hvset) #This will have Jaccard, plus others
 hvset@HVList$Intersection@Volume / hvset@HVList$Union@Volume #Can compute exactly as well
