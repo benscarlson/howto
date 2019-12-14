a487bf576ddf1ba713e630af926f7994be668a8c
Ben Carlson <ben.s.carlson@gmail.com>
2019-12-14 14:01:58 -0500

    Update charts.js

--- earthengine/charts.js
+++ earthengine/charts.js
@@ -6,3 +6,6 @@
 
 //Example of two time series
 //https://code.earthengine.google.com/3dc458d7be9b13ac4a41ef30a6bdeb0b
+
+//Can make any of the charts here
+//https://developers.google.com/chart/interactive/docs/gallery
