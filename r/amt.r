b989a3486b02554da4edf643fd9884cfd0db53dd
Ben Carlson <ben.s.carlson@gmail.com>
2020-01-13 14:02:43 -0500

    Update amt.r

--- r/amt.r
+++ r/amt.r
@@ -1,1 +1,7 @@
 library(amt)
+
+#See coded example from bioarxiv. Can be found in Zotero
+
+#---- workflow for single animal rsf ----#
+
+trk <- make_track(dat, lon, lat, timestamp, crs = sp::CRS("+init=epsg:4326"))
