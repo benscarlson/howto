8bdbc048884ea4ebfa91b67c539f9333d7ab1dcc
Ben Carlson <ben.s.carlson@gmail.com>
2020-01-22 14:06:47 -0500

    Update string_manipulation.r

--- r/string_manipulation.r
+++ r/string_manipulation.r
@@ -1,3 +1,4 @@
+trimws(s) #remove whitespace
 
 #---- extracting substrings ----
 stringr::str_extract('123ABC','\\d+') #will extract '123'. seems to work better than gsub
@@ -5,6 +6,9 @@ stringr::str_sub('123ABC',1,3) #First three chars '123'
 stringr::str_sub('123ABC',-3,-1) #Third from last to last 'ABC'
 
 sub('([^ ]+)_(\\d{4}$)','\\1','Adi_2_2017') #will print out the first matched group
+
+#if string is comma seperated, can just split
+str_split(s,',')
 
 #---- reverse string ----
 stringi::stri_reverse('123ABC') 
