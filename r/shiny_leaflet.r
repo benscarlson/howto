ed0e712d298bd5403a4767f718e0c68c8d00ebf2
Ben Carlson <ben.s.carlson@gmail.com>
2020-07-24 16:08:05 -0400

    Update shiny_leaflet.r

--- r/shiny_leaflet.r
+++ r/shiny_leaflet.r
@@ -1,7 +1,8 @@
 # Dynamic Leaflet maps in Shiny applications. https://riptutorial.com/r/example/16144/dynamic-leaflet-maps-in-shiny-applications
 # Shiny articles - reactivity. https://shiny.rstudio.com/articles/#reactivity
 
-#Simplest shiny app. File must be called app.R (can't be app.r)
+#---- Simplest shiny app ----#
+#File must be called app.R (can't be app.r)
 library(shiny)
 ui <- fluidPage(
   "Hello, world!"
@@ -10,6 +11,11 @@ server <- function(input, output, session) {
 }
 shinyApp(ui, server)
 
+#---- Layouts ----#
+
+fluidPage(splitLayout(htmlOutput('startDoy'),htmlOutput('endDoy'))) #put two elements side-by-side
+
+#---- Javascript ----#
 # Javascript function directly in ui declaration
 ui <- fluidPage(
   tags$script('
@@ -33,3 +39,19 @@ $(document).on("shiny:inputchanged", function(event) {
 });
 
 #---- Javascript Events ----#
+
+# event has name, value, and some other properties
+
+
+#How to set a UI element using javascript
+#This updates label with value of slider whenever slider moves
+
+#ui.r
+fluidpage(sliderInput('myslider'), htmlOutput('mylabel'));
+
+#script.js
+$(document).on("shiny:inputchanged", function(event) {
+  if (event.name === "myslider") {
+    document.getElementById("mylabel").innerHTML = event.value
+  }
+});
