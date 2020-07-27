338f4292d79c693d70f22d66472c0eba7304aa8a
Ben Carlson <ben.s.carlson@gmail.com>
2020-07-27 14:52:38 -0400

    Delete move.r

--- r/move.r
+++
@@ -1,4 +0,0 @@
-timestamps(mv) #get the timestamps of a move object
-timeLag(mv, units = 'mins')
-distance(mv) #returns vector of distances (in meters, for lon/lat) between points. vector length is one less than the number of points.
-angle(mv) #returns vector of angles between points. length is one less than number of points.
