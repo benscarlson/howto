963672f75677a902944470df4c4b798db11bfab6
Ben Carlson <ben.s.carlson@gmail.com>
2020-07-30 14:30:33 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
 
-#---- Dates/Times ----#
+//---- Dates/Times ----//
+new Date() //current date
+new Date(2019,0,1) //Jan 1 2019. Note month is 0-indexed, but day is not!
 
-new Date(2019,0,1) #Jan 1 2019. Note month is 0-indexed, but day is not!
+(new Date()).toString().split(' ')[1] //gets the name of the month
+(new Date()).getDate() //gets the day number (1-indexed)
