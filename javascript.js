4a95ae47034b37e5392752b1bef37fe3753b3862
Ben Carlson <ben.s.carlson@gmail.com>
2020-07-30 15:43:40 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@
 //---- Dates/Times ----//
 new Date() //current date
 new Date(2019,0,1) //Jan 1 2019. Note month is 0-indexed, but day is not!
+new Date(2019,0,365) //Create using doy. 365 is last day of year since 2019 is not a leap year and function is 1-indexed.
 
 (new Date()).toString().split(' ')[1] //gets the name of the month
 (new Date()).getDate() //gets the day number (1-indexed)
