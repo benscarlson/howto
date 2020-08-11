37f8153bf431698dcecd7a96d8a618c409165b1e
Ben Carlson <ben.s.carlson@gmail.com>
2020-08-11 14:42:39 -0400

    Update howto.sh

--- unix/howto.sh
+++ unix/howto.sh
@@ -4,7 +4,7 @@
 
 .bash_pofile #used for login shell (i.e remote in via ssh)
 .bashrc #used for non-login shell (i.e. opening another shell after you've already logged in)
-
+        #seems macos uses .bashrc and not .bash_profile
 dpkg -i debfile.deb #install a .deb file
 
 #---- create folders ----#
