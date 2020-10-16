9ca58f0e03c6d60e75c4975ecabb88881a4f5c9b
Ben Carlson <ben.s.carlson@gmail.com>
2020-10-16 11:01:42 -0400

    Update modeling_packages.r

--- r/modeling_packages.r
+++ r/modeling_packages.r
@@ -11,5 +11,5 @@ str(ranef(m)$cond) #individual model parameters are in cond slot
 slopes <- ranef(m)$cond$niche_name %>%
   rownames_to_column(var='niche_name')
 
-ggplot(slopes,aes(x=niche_name,y=tree)) +
+ggplot(slopes,aes(x=tree,y=niche_name)) +
   geom_point()
