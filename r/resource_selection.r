98d6e52385bdacad6421cd92c5cbb369a3634d8b
Ben Carlson <ben.s.carlson@gmail.com>
2021-03-30 14:42:29 -0400

    Update resource_selection.r

--- r/resource_selection.r
+++ r/resource_selection.r
@@ -1,1 +1,12 @@
 
+#---------------------#
+#---- Muff et al. ----#
+#---------------------#
+
+#Model requires stratum_id. 
+#Amt provides step_id_ that identifies each step and all the potential background steps associated with that step
+#But model requires stratum id that is unique across all animals
+#This can be created by combining the id or name of the animal with the stratum.
+
+mutate(stratum_id=paste(niche_name,step_id_,sep='_'))
+    
