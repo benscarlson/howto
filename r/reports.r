ec96e091c631dd5744b1672628fcc442d4295c2a
Ben Carlson <ben.s.carlson@gmail.com>
2021-07-09 15:11:08 -0400

    Update reports.r

--- r/reports.r
+++ r/reports.r
@@ -188,7 +188,24 @@ https://www.sharelatex.com/learn/Page_size_and_margins
 #https://tex.stackexchange.com/questions/337083/latex-table-of-contents-not-visible-in-sidebar-of-pdf-viewer
 #
 
+#---- Merge multiple files to pdf ----#
+
 #---- CPDF ----#
 # Use to combine multiple pdfs from command line. Lots of features:
 # http://community.coherentpdf.com/
 
+#---- pngs -> pdf ----#
+
+#Merges png files into one pdf
+dir=$wd/segtest2/reports/donana
+
+files=`ls $dir/png/`
+
+mkdir $dir/png_off
+
+for file in $files
+do
+  convert $dir/png/$file -background white -alpha remove -alpha off $dir/png_off/$file
+done
+
+img2pdf $dir/png_off/* -o $dir/bpm_donana.pdf
