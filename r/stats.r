55c8a0e4539f72ac9ee7159ff8bc513214053311
Ben Carlson <ben.s.carlson@gmail.com>
2021-08-20 18:24:47 -0400

    Update stats.r

--- r/stats.r
+++ r/stats.r
@@ -2,3 +2,9 @@ m <- lm(y~x)
 
 predict(m,interval="confidence") #confidence intervals. default is two-sided 95%
 predict(m,se.fit=TRUE) #standard errors
+
+#Get the p-value for one-way anova
+a <- aov(niche_vol~loc_label,data=gdat)
+summary(a)
+#p-value is buried. summary(a)[[1]] is a dataframe
+pval <- summary(a)[[1]]['loc_label','Pr(>F)']
