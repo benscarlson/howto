bd910bfd281bbc3d336be67b70d591f92528f7b5
Ben Carlson <ben.s.carlson@gmail.com>
2022-10-10 13:11:20 -0400

    Update scripting.r

--- r/scripting.r
+++ r/scripting.r
@@ -14,4 +14,17 @@ args <- commandArgs(trailingOnly=TRUE)
 datName <- args[1]
 if(is.na(datName)) stop('datName required')
 
+#---- docopt ----#
 
+#For boolean option, need to have more than one space (or a tab) after the variable or it will not work correctly
+
+#One space
+-u --use Use an existing session
+# * will be null if not set
+# * will throw an error if set
+
+#Two spaces or a tab
+-u --use  Use an existing session
+
+#boolean values are already logical data type, dont need to set
+as.logical(ag$use) #Don't need to do this
