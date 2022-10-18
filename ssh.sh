1f8ab3246592d7bb9e6f71132d6807f64face6fb
Ben Carlson <ben.s.carlson@gmail.com>
2022-10-18 14:38:52 -0400

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,5 +1,8 @@
 ~/.ssh/id_rsa #private key
 ~/.ssh/id_rsa.pub #public key
 
+#For additional keys, add the host name as a suffix. e.g.
+~/.ssh/id_rsa_grace #private key
+
 #delte an entry from known_hosts
 ssh-keygen -R hostname
