bfe688bb3e7c8482518d3e9bf7faa0d16c8eed58
Ben Carlson <ben.s.carlson@gmail.com>
2023-01-26 21:52:38 -0500

    Create brms.r

---
+++ r/brms.r
@@ -0,0 +1,2 @@
+# brmstools: https://mvuorre.github.io/brmstools/index.html
+# brmstools spaghetti plots: https://mvuorre.github.io/brmstools/articles/panel-spaghetti-plots/panel-spaghetti-plots.html
