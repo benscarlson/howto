c7b498a5b7bd68d2e805b4b9a75ae36c1ab72734
Ben Carlson <ben.s.carlson@gmail.com>
2024-02-02 15:39:09 -0500

    Update spatial_terra.r

--- r/spatial_terra.r
+++ r/spatial_terra.r
@@ -5,3 +5,15 @@ cat(crs(r1)) #Print crs in WKT format
 crs(r1,proj=TRUE) #Print crs in proj format
 
 describe('path/to/rast.tif') #GDALinfo
+
+datatype(r)
+
+res(rLulc) #Print x,y resolution
+
+#Assign labels to an categorical raster
+
+#!!! Note **MUST** be dataframe. It will give a strange error 
+classes <- data.frame(id=0:6,
+  cover=c('NoData','Cropland','Forest','Grassland','Urban','Barren','Water'))
+
+levels(rLulc) <- classes
