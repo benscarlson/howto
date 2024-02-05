2a7268efd1cfab2568b8b54afb2ac0c82a36cb15
Ben Carlson <ben.s.carlson@gmail.com>
2024-02-05 16:57:03 -0500

    Update spatial_terra.r

--- r/spatial_terra.r
+++ r/spatial_terra.r
@@ -4,11 +4,15 @@
 cat(crs(r1)) #Print crs in WKT format
 crs(r1,proj=TRUE) #Print crs in proj format
 
-describe('path/to/rast.tif') #GDALinfo
+describe('path/to/rast.tif') #GDALinfo. #Sometimes does not print in RStudio but prints when running the report
 
-datatype(r)
+#INT/FLT integer/float; 1-4 num bytes; S/U signed/unsigned.
+#See: ?writeRaster
+datatype(r) 
 
 res(rLulc) #Print x,y resolution
+
+gdal() #See the gdal version
 
 #Assign labels to an categorical raster
 
