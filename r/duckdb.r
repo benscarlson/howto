d0aa6b1132c731372f7e5d7210beb3a22983ef12
Ben Carlson <ben.s.carlson@gmail.com>
2024-02-26 15:10:39 -0500

    Update duckdb.r

--- r/duckdb.r
+++ r/duckdb.r
@@ -1,1 +1,4 @@
 dbConnect(duckdb(), .dbPF, read_only=TRUE) # Open in read only mode. Seems to not create a lock file
+
+#To select a column that is a reserved keyword
+"select spid, \"group\" from loc l where data_type='pa'"
