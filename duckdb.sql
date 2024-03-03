1ce3eb0afe90cdb24d3b596bf3865b6f5418e589
Ben Carlson <ben.s.carlson@gmail.com>
2024-03-03 00:05:31 -0500

    Update duckdb.sql

--- duckdb.sql
+++ duckdb.sql
@@ -1,1 +1,8 @@
 -- To use a reserved word as a column defintion when creating a table, enclose the column name with "". eg. 'group'
+
+-- How to use a sequence as the default value for a primary key
+create sequence seq_my_id start 1;
+
+create table mytb (
+  my_id ubigint primary key default nextval('seq_my_id'), 
+);
