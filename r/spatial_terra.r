2deb86545c749527d6a26c393fe8f9fdc4cf2b02
Ben Carlson <ben.s.carlson@gmail.com>
2024-03-08 12:52:29 -0500

    Update spatial_terra.r

--- r/spatial_terra.r
+++ r/spatial_terra.r
@@ -1,4 +1,12 @@
 
+#---- Creating rasters ----
+
+rast(nrow=100,ncol=100) #This assumes a wgs84 grid, e.g -180, 180, -90, 90. Sets resolution based on nrow/ncol
+
+rast(nrow=100,ncol=100,xmin=0,xmax=100,ymin=0,ymax=100) #Specify xmin etc to control the grid.
+
+rast(matrix(1:4,nrow=2,ncol=2)) #make a raster from a matrix
+
 ## Raster Info
 
 cat(crs(r1)) #Print crs in WKT format
@@ -22,3 +30,17 @@ classes <- data.frame(id=0:6,
   cover=c('NoData','Cropland','Forest','Grassland','Urban','Barren','Water'))
 
 levels(rLulc) <- classes
+
+#----
+#---- Manually changing raster values ----
+#----
+
+tpl <- rast(nrow=100,ncol=100,xmin=0,xmax=100,ymin=0,ymax=100)
+
+#updating according to a matrix. (0, 0) is the top left corner
+# rows is y. 0 starts at the top
+# columns is x. 0 starts on the left
+env <- tpl; env[60:80,40:50] <- 1; plot(env) #The y-scale is confusing because it should start from the top
+env <- tpl; env[40:50,60:80] <- 1; plot(env) #The y-scale is confusing because it should start from the top
+
+
