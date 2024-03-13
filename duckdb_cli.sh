4475b39b26ce66a1c6beae981c9b3571d22eee57
Ben Carlson <ben.s.carlson@gmail.com>
2024-03-13 15:18:18 -0400

    Update duckdb_cli.sh

--- duckdb_cli.sh
+++ duckdb_cli.sh
@@ -7,3 +7,15 @@ duckdb $db < $src/db/database.sql
 #---- duckdb interpreter ----
 
 show tables; #show all tables
+
+#---- Load a csv file when a table has an auto-generating primary key
+
+create table temp_tbl as from 
+  read_csv_auto('~/projects/disdat_db/data/spp_exceptions.csv',
+    header=true);
+    
+insert into spp_exceptions (spex_id,spid,origin,reason)
+select nextval('seq_spex_id') as spex_id, spid, origin, reason
+from temp_tbl;
+
+drop table temp_tbl;
